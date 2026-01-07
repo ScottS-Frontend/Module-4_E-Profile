e0480d63250fbbf6b1792e0dce544c57d8de6ab6
Scott Slagle <spsbiz@hotmail.com>
2026-01-06 16:58:23 -0800

    Completed Integrating Email JS

---
+++ index.js
@@ -0,0 +1,26 @@
+// template_jygyeph
+// service_meetweg
+// RJthz35oIiV7VG5Ey
+
+function contact() {
+  event.preventDefault(event);
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+
+    emailjs
+     .sendForm(
+      "service_meetweg",
+      "template_jygyeph",
+      event.target,
+      "RJthz35oIiV7VG5Ey"
+    ).then(() => {
+         loading.classList.remove("modal__overlay--visible");
+         success.classList += " modal__overlay--visible";    
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly at scottslaglebusiness@gmail.com"
+        );
+    })
+}
