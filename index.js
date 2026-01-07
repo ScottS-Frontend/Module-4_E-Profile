22b678d8073404d7317b12e63770493eef4c400a
Scott Slagle <spsbiz@hotmail.com>
2026-01-07 14:12:52 -0800

    Completed Background Shapes

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-// template_jygyeph
-// service_meetweg
-// RJthz35oIiV7VG5Ey
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
 
-function contact() {
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+  }
+}
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
+}
+
+function contact(event) {
   event.preventDefault(event);
   const loading = document.querySelector(".modal__overlay--loading");
   const success = document.querySelector(".modal__overlay--success");
@@ -26,13 +47,11 @@ function contact() {
     });
 }
 
-let isModalOpen = false;
-function toggleModal() {    
-    if (isModalOpen) {
-        isModalOpen = false;
-        return document.body.classList.remove("modal--open")
-        
-    }   
-    isModalOpen = true; 
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
   document.body.classList += " modal--open";
 }
